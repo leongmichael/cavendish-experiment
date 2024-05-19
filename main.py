ae4d07e2d163f2cdf1880871b114c1a6e96f77ef
Jonathan Ouyang <jonsouyang@gmail.com>
2024-05-19 01:20:31 -0700

    Finished derivation of G animation code. will add more documentation tomorrow

--- main.py
+++ main.py
@@ -93,45 +93,92 @@ class setup(ThreeDScene):
 class deriveG(Scene):
     def construct(self):
 
-        
+        #Line 1 0 = FgL - ktheta
+        t01 = MathTex(r'\tau_{net}')
+        t02 = MathTex(r'=')
+        t03 = MathTex(r'\tau_{gravity}')
+        t041 = MathTex(r'+')
+        t042 = MathTex(r'\tau_{wire}')
+        t04 = VGroup(t041, t042).arrange(RIGHT, buff =0.5)
+        t0 = VGroup(t01, t02, t03, t04).arrange(RIGHT, buff = 0.2)
+        self.play(Create(t0))
+        self.wait(0.5)
+        temp1 = MathTex(r'F_gL')
+        temp_copy = temp1.copy().move_to(t0[2].get_center())
+        self.play(ReplacementTransform(t0[2], temp_copy))
+        self.wait(0.5)
+        t041n = MathTex(r'-')
+        t042n = MathTex(r'k\theta')
+        temp2 = VGroup(t041n, t042n).arrange(RIGHT, buff=0.5)
+        # Create a temporary copy with the same position (idk why this works)
+        temp_copy = temp2.copy().move_to(t0[3].get_center())
+        # Perform replacement and remove the copy
+        self.play(ReplacementTransform(t0[3], temp_copy),)
+        t0.add(temp_copy)
+        self.wait(1)
+        self.play(FadeOut(t0, run_time=0.01))
 
-        tau = MathTex(r'\tau_{net}')
-        tau2 = MathTex(r' = F_gL - k\theta')
+        #tau = MathTex(r'\tau_{net}')
+        #tau2 = MathTex(r' = F_gL - k\theta')
         zero = MathTex(r'0')
-        zero.move_to(UP * 3.05 + LEFT * 1.2)
-        t1 = VGroup(tau, tau2).arrange(RIGHT, buff=0.5)
-        self.play(Create(t1))
-        self.play(t1.animate.shift(UP*3))
-        self.play(FadeOut(tau))
+        zero.move_to(UP * 2.85 + LEFT * 2.2)
+
+        self.play(t0.animate.shift(UP*2.8))
+        self.play(FadeOut(t0[0]))
         self.play(FadeIn(zero))
+        self.wait(1)
 
-
+        #Line 2 transform Fg = Ktheta/L
+        path =  ArcBetweenPoints(temp_copy.get_center(), zero.get_center()+LEFT*0.5, angle=PI/2, stroke_width=8)
+        #temp_copy.remove(t041n)
+        #self.play(FadeOut(t041n), run_time=0.5)
+        #self.remove(t041n)
+        self.play(FadeOut(temp_copy[0]), run_time=0.3)
+        temp_copy.remove(temp_copy[0])
+        self.play(MoveAlongPath(temp_copy, path), FadeOut(zero), run_time=1)
         
+        #clean equation t0 to reflect new equation
+        t0.remove(t01)
+        self.play(t0.animate.shift(ORIGIN))
+
+        #line 2, 3, 4
+        t2 = MathTex(r" \frac{k\theta}{L}")
+        t3 = MathTex(r"\frac{GMm}{r^2}")
+        r_transform = VGroup(t2, MathTex("="), MathTex(r"F_g"), MathTex("="), t3)
+        r_transform.arrange(direction=RIGHT, buff=1)
+        r_transform.next_to(t0, DOWN*1.2)
+        r_transform.move_to(r_transform.get_center()+RIGHT*1.85)
+        self.play(Create(r_transform[:3]))
         self.wait(1)
-        t2 = MathTex(r"F_g = \frac{k\theta}{L}")
-        t2.next_to(t1, DOWN)
-        self.play(Create(t2))
+        self.play(Create(r_transform[3:]))
+        self.wait(1)
+
+        # The mobs replace each other and none are left behind
+        self.play(FadeOut(r_transform[2]), FadeOut(r_transform[3]))
+        self.play(r_transform[4].animate.shift(LEFT*3))
+        r_transform.remove(r_transform[2])
+        r_transform.remove(r_transform[3])
 
         self.wait(1)
-        t3 = MathTex(r"F_g = \frac{GMm}{r^2}")
-        t3.next_to(t2, DOWN)
-        self.play(Create(t3))
-
-        self.wait(1)
-        t4 = MathTex(r"\frac{k\theta}{L} = \frac{GMm}{r^2}")
-        t4.next_to(t3, DOWN)
-        self.play(Create(t4))
-
-        self.wait(1)
-        t5 = MathTex(r"G = \frac{k\theta r^2}{LMm}")
-        t5.next_to(t4, DOWN)
+        t5 = MathTex(r"\frac{k\theta r^2}{LMm} \;=\; G")
+        t5.next_to(r_transform, DOWN*1.4)
+        t5.move_to(t5.get_center()+LEFT*1.1)
         self.play(Create(t5))
 
         self.wait(1)
-        t6 = MathTex(r"G = \frac{\frac{m_{rod}L^2}{12}+2m(\frac{L}{2})^2(\frac{2\pi}{T})^2\theta r^2}{LMm}")
-        t6.next_to(t5, DOWN)
-        self.play(Create(t6))
-        self.wait(2)
+        t6 = MathTex(r"\frac{(\frac{m_{rod}(L)^2}{12}+2m(\frac{L}{2})^2)(\frac{2\pi}{T})^2\theta r^2}{LMm} \;=\; G")
+        t6.next_to(t5, DOWN*2)
+        #t6.move_to(t6.get_center()+LEFT*2)
+        self.play(Create(t6, run_time=4))
+        self.wait(1)
+
+        #box answer
+        box = SurroundingRectangle(t6, color=BLUE, buff=0.3, corner_radius=0.1)
+
+        # Add both the equation and the box to the scene
+        self.add(box, t6)
+        self.play(Create(box))
+        self.wait(3)
 
 class testArc(Scene):
 
@@ -170,34 +217,21 @@ class testArc(Scene):
         self.play(equation1.animate.shift(RIGHT + UP))
         self.wait(1)
 
-""" broken method
-class FadeInExample(Scene):
+class testCombine(Scene):
     def construct(self):
+        r_transform = VGroup(Text("a "), Text("="), Text("b"), Text("="), Text("c"))
+
+        r_transform.arrange(direction=RIGHT, buff=1)
+        self.play(Create(r_transform))
+
+
+        # The mobs replace each other and none are left behind
+        self.play(FadeOut(r_transform[2]), FadeOut(r_transform[3]))
+        self.play(r_transform[4].animate.shift(LEFT*3))
+        r_transform.remove(r_transform[2])
+        r_transform.remove(r_transform[3])
+        t4 = MathTex(r"\frac{k\theta}{L} = \frac{GMm}{r^2}")
+        t4.next_to(r_transform, DOWN)
+        self.play(Create(t4))
+
         
-        tex1 = Tex("a", " = ", "b ", " + ",  "c").scale(1)
-        equation1 = VGroup(tex1)
-        self.play(Write(equation1))
-
-        tex2 = Tex("a", " - ", "c ", " = ",  "b").scale(1)
-        equation2 = VGroup(tex2).arrange(RIGHT, buff=0.5)
-        minus = Text(" - ")
-        minus.move_to(LEFT*1.2)
-
-
-        animations = [
-            (tex1[0].animate.shift(tex2[0].get_center()),
-            tex1[1].animate.shift(tex2[3].get_center()),
-            tex1[2].animate.shift(tex2[4].get_center()),
-            MoveAlongPath(tex1[4], ArcBetweenPoints(tex1[4].get_center(), tex2[1].get_center() + LEFT * 0.1, angle=PI/2, stroke_width=8)),
-            FadeOut(tex1[3]),
-
-            FadeIn(minus))
-        ]
-        for a in animations:
-            self.play(a)
-        equation2.add(minus)
-        #self.play(AnimationGroup(*animations, lag_ratio=0.5))
-        self.wait(1)
-        self.play(equation2.animate.shift(UP*2))
-        self.wait(1)
-"""
